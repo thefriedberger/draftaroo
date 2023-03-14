7058341378dfedf962a20cee5e4f213299121cd7
Tyler Friedberg <thefriedberger@gmail.com>
2023-03-14 10:51:28 -0400

    removing some files

--- src/util/supabaseActions.tsx
+++
@@ -1,8 +0,0 @@
-import * as Types from "./types";
-import { supabase } from "@/lib/supabaseClient";
-
-export const draftPlayer = (player: Types.Player, team: Types.Team) => {};
-
-export const watchPlayer = (player: Types.Player, team: Types.Team) => {};
-
-export const getTeams = () => {};
