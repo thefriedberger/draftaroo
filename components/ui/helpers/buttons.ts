7943eb7c3ac52e058f1655cf12ed20989fcb9b3b
Tyler Friedberg <thefriedberger@gmail.com>
2024-09-05 10:04:01 -0400

    chore: button dark bg primary gray

--- components/ui/helpers/buttons.ts
+++ components/ui/helpers/buttons.ts
@@ -1,2 +1,2 @@
 export const buttonClasses =
-   'py-2 px-4 rounded-md no-underline bg-paper-button hover:bg-paper-light dark:bg-gray-light dark:hover:bg-gray-primary dark:text-white text-sm';
+   'py-2 px-4 rounded-md no-underline bg-paper-button hover:bg-paper-light dark:bg-gray-primary dark:hover:bg-gray-light dark:text-white text-sm';
