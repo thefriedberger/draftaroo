3b2511b0501c41beadbfe84b5c25dd6e7b705535
Tyler Friedberg <thefriedberger@gmail.com>
2024-09-05 10:01:19 -0400

    chore: changing button dark bg color

--- components/ui/helpers/buttons.ts
+++ components/ui/helpers/buttons.ts
@@ -1,2 +1,2 @@
 export const buttonClasses =
-   'py-2 px-4 rounded-md no-underline bg-paper-button hover:bg-paper-light dark:bg-gray-dark dark:hover:bg-gray-primary dark:text-white text-sm';
+   'py-2 px-4 rounded-md no-underline bg-paper-button hover:bg-paper-light dark:bg-gray-light dark:hover:bg-gray-primary dark:text-white text-sm';
