85a6a339bb253159908e9882b12962bb2493b2f4
Tyler Friedberg <thefriedberger@gmail.com>
2024-09-23 14:37:53 -0400

    chore: trying to move timer time to server

--- supabase/functions/timer/index.ts
+++ supabase/functions/timer/index.ts
@@ -1,6 +1,7 @@
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
+// @ts-ignore-strict
 
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 
