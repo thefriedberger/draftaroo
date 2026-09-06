7165c44f189a810b538ef8eacae9e0cc770e3b3c
Tyler <thefriedberger@gmail.com>
2026-09-06 12:32:53 -0400

    fix: rounding removed from projected scores

--- app/utils/project-stats.ts
+++ app/utils/project-stats.ts
@@ -21,13 +21,26 @@ const projectStats = (player: Player) => {
                ) {
                   projected[projectedYear][key] = 0;
                } else {
-                  projected[projectedYear][key] = Math.floor(
-                     Math.round(
-                        (Math.round((statsB[key] + (statsA?.[key] || 0)) * 10) /
-                           10 /
-                           numberOfSeasons || 3) * 10
-                     ) / 10
-                  );
+                  if (key === 'averageScore' || key === 'score') {
+                     projected[projectedYear][key] =
+                        Math.round(
+                           (Math.round(
+                              (statsB[key] + (statsA?.[key] || 0)) * 10
+                           ) /
+                              10 /
+                              numberOfSeasons || 3) * 10
+                        ) / 10;
+                  } else {
+                     projected[projectedYear][key] = Math.floor(
+                        Math.round(
+                           (Math.round(
+                              (statsB[key] + (statsA?.[key] || 0)) * 10
+                           ) /
+                              10 /
+                              numberOfSeasons || 3) * 10
+                        ) / 10
+                     );
+                  }
                }
             } else {
                if (numberOfSeasons === 1 && key !== 'games') {
