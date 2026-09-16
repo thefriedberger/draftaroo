2b97bc0f91039429f7f92ebf00eb956f0f047926
Tyler <thefriedberger@gmail.com>
2026-09-16 18:00:19 -0400

    chore: more draft tile adjustments

--- components/ui/draft/components/draft-tile/index.tsx
+++ components/ui/draft/components/draft-tile/index.tsx
@@ -53,8 +53,10 @@ const DraftTile = ({ pick, currentPick, player }: DraftTileProps) => {
          onClick={handleUpdateFeaturedPlayer}
          data-featured-toggle={true}
       >
-         <div className="flex justify-between bg-[rgba(0,0,0,.25)] rounded-t-[4px] h-[calc(fit-content-2px)] mt-[2px] w-[calc(100%-4px)] ml-[2px] px-1">
-            <span className={''}>{pick.draftPosition}</span>
+         <div className="flex justify-between bg-[rgba(0,0,0,.25)] rounded-t-[4px] h-[calc(fit-content-2px)] mt-[2px] w-[calc(100%-4px)] ml-[2px] px-1 pr-0">
+            <span className={'text-white font-medium'}>
+               {pick.draftPosition}
+            </span>
             <span className="flex items-center">
                {pick.isKeeper && <KeeperIcon />}
             </span>
